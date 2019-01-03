02d3c86e679741733d46e9c177b4ebdfbd4f1724
Fridtjof <s0530085@htw-berlin.de>
2019-01-03 15:32:30 +0100

    aufgeräumt, Verzeichnis ui in root-verzeichnis verschoben

--- js/main.js
+++ js/main.js
@@ -1,27 +1,21 @@
-let audioCtx = new AudioContext();
+import webAudioManager from './webAudioManager.js';
+import AudioPlayer from './audioPlayer.js';
 
-var gainNode =audioCtx.createGain();
 
-gainNode.connect(audioCtx.destination);
-let source = audioCtx.createBufferSource();
+let audioPlayer1 = new AudioPlayer("../media/Freischuetz.wav");
+let audioPlayer2 = new AudioPlayer("../media/basic_beat.wav");
 
-let request = new XMLHttpRequest();
 
-request.open('GET', './basic_beat.wav', true);
-request.responseType = 'arraybuffer';
+document.getElementById("playBtn1").onclick = audioPlayer1.onTogglePlay.bind(audioPlayer1);
+document.getElementById("volumeRange1").onchange = audioPlayer1.onVolumeChange.bind(audioPlayer1);
+document.getElementById("playBtn2").onclick = audioPlayer1.onTogglePlay.bind(audioPlayer2);
+document.getElementById("volumeRange2").onchange = audioPlayer1.onVolumeChange.bind(audioPlayer2);
 
-request.onload = function() {
-    let audioData = request.response;
 
-    audioCtx.decodeAudioData(audioData, function(buffer) {
-        source.buffer = buffer;
-        source.connect(gainNode);
-        source.loop = true;
-      //  source.start(0);
-    });
+
+var volumeRange1 = document.getElementById("volumeRange1");
+$('#default').puiinputtextarea();
+
+volumeRange1.oninput = () => {
+    console.log(volumeRange1.value);
 };
-document.getElementById('volume').addEventListener('change', function () {
-  gainNode.gain.value = this.value;
- });
-
-request.send();
