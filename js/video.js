c9e1395402ec808aa5ba1a18c0f9f69ef810eed3
Florian Stallmach <you@example.com>
2019-02-02 14:48:41 +0100

    Play two Videos at the same time

--- js/video.js
+++ js/video.js
@@ -2,5 +2,5 @@ import VideoPlayer from './videoPlayer.js';
 
 const videoElement1 = document.querySelector('#videoplayer1');
 let videoplayer1 = new VideoPlayer("../media/video.mpd", videoElement1, true);
-//const videoElement2 = document.querySelector('#videoplayer2');
-//let videoplayer2 = new VideoPlayer("../media/video.mpd", videoElement1);
+const videoElement2 = document.querySelector('#videoplayer2');
+let videoplayer2 = new VideoPlayer("../media/video.mpd", videoElement2, true);
